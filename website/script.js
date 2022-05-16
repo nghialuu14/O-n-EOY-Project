f605fc0ade3312bdde3374b4ada89d01fc1cd02b
Janice Liang <66046716+aienemone@users.noreply.github.com>
2022-05-16 14:03:30 -0500

    Add files via upload

--- website/script.js
+++ website/script.js
@@ -1,26 +1,4 @@
 // :( 
-// character 
-  // html div element, img element, event listeners in jsw file, boundaries of website, boundaries at tombstone, when tombstone pops up - event listener (when player is at tombstone coordinate), 
-
-/*
-===
-JANICE LIANG
-PICTURE
-DATE 
-CAUSE OF DEATH 
-===== 
-
-FUNCTIONS
-
-character movement
-- eventlistener 
-
-window.eventlistener
-- check which key pressed
-- branch to substatements
-main {
-}
-*/
 
 const SCALE = 1.5;
 const WIDTH = 46;
@@ -41,8 +19,8 @@ let keyPresses = {};
 let currentDirection = FACING_DOWN;
 let currentLoopIndex = 0;
 let frameCount = 0;
-let positionX = 401;
-let positionY = 603;
+let positionX = 429;
+let positionY = 517;
 let img = new Image();
 
 window.addEventListener('keydown', keyDownListener);
@@ -74,7 +52,7 @@ loadChar();
 
 // this one is the top left grave
 function nearA(px, py) {
-  if (px > 196 && px < 230 && py > 142 && py < 200) {
+  if (px > 180 && px < 220 && py > 151 && py < 183) {
     return true;
   }
   return false;
@@ -83,7 +61,7 @@ function nearA(px, py) {
 // TOP MIDDLE GRAVE
 // 317, 150
 function nearB(px, py) {
-  if (px > 449 && px < 483 && py > 142 && py < 200) {
+  if (px > 424 && px < 452 && py > 151 && py < 183) {
     return true;
   }
   return false;
@@ -91,7 +69,7 @@ function nearB(px, py) {
 
 //TOP RIGHT GRAVE
 function nearC(px, py) {
-  if (px > 701 && px < 729 && py > 142 && py < 200) {
+  if (px > 677 && px < 710 && py > 142 && py < 200) {
     return true;
   }
   return false;
@@ -99,7 +77,7 @@ function nearC(px, py) {
 
 //BOTTOM LEFT GRAVE
 function nearD(px, py) {
-  if (px > 196 && px < 230 && py > 287 && py < 334) {
+  if (px > 175 && px < 220 && py > 286 && py < 340) {
     return true;
   }
   return false;
@@ -107,7 +85,7 @@ function nearD(px, py) {
 
 //BOTTOM MIDDLE GRAVE
 function nearE(px, py) {
-  if (px > 449 && px < 483 && py > 287 && py < 334) {
+  if (px > 424 && px < 483 && py > 287 && py < 340) {
     return true;
   }
   return false;
@@ -115,7 +93,7 @@ function nearE(px, py) {
 
 //BOTTOM RIGHT GRAVE
 function nearF(px, py) {
-  if (px > 701 && px < 729 && py > 287 && py < 334) {
+  if (px > 681 && px < 729 && py > 287 && py < 340) {
     return true;
   }
   return false;
@@ -123,7 +101,7 @@ function nearF(px, py) {
 
 //RIGHT MAUSOLEUM
 function nearML(px, py) {
-  if (px > 558 && px < 566 && py > 435 && py < 461) {
+  if (px > 555 && px < 630 && py > 400 && py < 464) {
     return true;
   }
   return false;
@@ -131,7 +109,7 @@ function nearML(px, py) {
 
 //LEFT MAUSOLEUM
 function nearGame(px, py) {
-  if (px > 292 && px < 352 && py > 427 && py < 464) {
+  if (px > 281 && px < 357 && py > 400 && py < 464) {
     return true;
   }
   return false;
@@ -144,8 +122,11 @@ function checkKey(e) {
     window.open('http://dev.moorman.xyz/', '_blank');
   }
   else if (e.keyCode == '88') {
-    // will replace link
     window.open('game.html', '_blank');
+  }
+  else if (e.keyCode == '67') {
+    // will replace link
+    window.open('intro.html', '_blank');
   }
 }
 
@@ -154,32 +135,42 @@ function gameLoop() {
   ctx.clearRect(0, 0, canvas.width, canvas.height);
 
   let tb = new Image();
-  tb.src = 'imgs/tb.png';
+  tb.src = 'imgs/tbs/tb.png';
   // top left grave - 100, 130
-  ctx.drawImage(tb, 200, 160, 60, 60);
+  ctx.drawImage(tb, 200, 160, 70, 75);
   // top middle - 300, 130
-  ctx.drawImage(tb, 450, 160, 60, 60);
+  ctx.drawImage(tb, 450, 160, 70, 75);
   //top right grave - 500, 130
-  ctx.drawImage(tb, 700, 160, 60, 60);
-  ctx.drawImage(tb, 200, 300, 60, 60);
-  ctx.drawImage(tb, 450, 300, 60, 60);
-  ctx.drawImage(tb, 700, 300, 60, 60);
-  let maus = new Image();
-  maus.src = 'imgs/maus.png';
+  ctx.drawImage(tb, 700, 160, 70, 75);
+  ctx.drawImage(tb, 200, 300, 70, 75);
+  ctx.drawImage(tb, 450, 300, 70, 75);
+  ctx.drawImage(tb, 700, 300, 70, 75);
+  let mausl = new Image();
+  let mausr = new Image();
+  mausl.src = 'imgs/mausl.png';
   // game (left)
-  ctx.drawImage(maus, 290, 427, 90, 80);
+  ctx.drawImage(mausl, 290, 400, 130, 100);
+  mausr.src = 'imgs/mausr.png';
   // ml (right)
-  ctx.drawImage(maus, 560, 427, 90, 80);
-
-  let tbsign = new Image();
-  tbsign.src = 'imgs/tbsign.png'
-  ctx.drawImage(tbsign, -10, 430, 250, 280);
+  ctx.drawImage(mausr, 560, 375, 150, 135);
 
   let lampl = new Image();
   lampl.src = 'imgs/street-lamp.png'
-  ctx.drawImage(lampl, -10, 620, 80, 80);
+  ctx.drawImage(lampl, 40, 300, 55, 260);
 
-  ctx.drawImage(lampl, -10, 100, 80, 80);
+  let banner = new Image();
+  banner.src = 'imgs/banner.png'
+  ctx.drawImage(banner, 190, 60, 600, 70);
+
+  ctx.drawImage(lampl, 870, 300, 55, 260);
+
+  let tbsign = new Image();
+  tbsign.src = 'imgs/tbsign.png'
+  ctx.drawImage(tbsign, 60, 430, 250, 280);
+
+  let seeon = new Image();
+  seeon.src = 'imgs/seeon.png'
+  ctx.drawImage(seeon, 800, 100, 190, 90);
   
   let hasMoved = false;
 
@@ -221,41 +212,41 @@ function gameLoop() {
     //console.log("true!");
     let mod1 = new Image();
     mod1.src = 'imgs/tbs/tbsami.png';
-    ctx.drawImage(mod1, 300, 80, 470, 600);
+    ctx.drawImage(mod1, 300, 40, 450, 600);
   }
   // for top middle grave
   if (nearB(positionX, positionY) == true) {
     let mod1 = new Image();
     mod1.src = 'imgs/tbs/tbcal.png';
-    ctx.drawImage(mod1, 300, 100, 470, 600);
+    ctx.drawImage(mod1, 300, 40, 450, 600);
   }
 
   //for top right grave
   if (nearC(positionX, positionY) == true) {
     let mod1 = new Image();
     mod1.src = 'imgs/tbs/tbtori.png';
-    ctx.drawImage(mod1, 300, 100, 470, 600);
+    ctx.drawImage(mod1, 300, 40, 450, 600);
+  }
+
+  //for bottom left grave
+  if (nearD(positionX, positionY) == true) {
+    let mod1 = new Image();
+    mod1.src = 'imgs/tbs/tbjen.png';
+    ctx.drawImage(mod1, 300, 40, 450, 600);
   }
 
   //for bottom middle grave
-  if (nearD(positionX, positionY) == true) {
-    let mod1 = new Image();
-    mod1.src = 'imgs/tbs/testzoom.png';
-    ctx.drawImage(mod1, 300, 100, 470, 600);
-  }
-
-  //for top right grave
   if (nearE(positionX, positionY) == true) {
     let mod1 = new Image();
-    mod1.src = 'imgs/tbs/testzoom.png';
-    ctx.drawImage(mod1, 300, 100, 470, 600);
+    mod1.src = 'imgs/tbs/tbjan.png';
+    ctx.drawImage(mod1, 300, 40, 450, 600);
   }  
 
-  //for top right grave
+  //for bottom right grave
   if (nearF(positionX, positionY) == true) {
     let mod1 = new Image();
-    mod1.src = 'imgs/tbs/testzoom.png';
-    ctx.drawImage(mod1, 300, 100, 470, 600);
+    mod1.src = 'imgs/tbs/tbhuyn.png';
+    ctx.drawImage(mod1, 300, 70, 470, 600);
   }  
 
   // RIGHT MOSOLEUM 
@@ -273,11 +264,8 @@ function gameLoop() {
     gamepop.src = 'imgs/gamesign.png';
     ctx.drawImage(gamepop, 230, 485);
   }
-
-  
-  console.log(positionX, positionY);
+  // console.log(positionX, positionY);
 }
-
 
 function moveCharacter(deltaX, deltaY, direction) {
   if (positionX + deltaX > 0 && positionX + SCALED_WIDTH + deltaX < canvas.width) {
@@ -288,10 +276,3 @@ function moveCharacter(deltaX, deltaY, direction) {
   }
   currentDirection = direction;
 }
-
-
-//modal for sami
-
-// var modal_1 = mod1.src = '../imgs/testzoom.png';
-
-// code for 
